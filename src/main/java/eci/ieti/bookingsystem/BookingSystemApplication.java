78b0699461e8a174c8e566229b3562220106cbb2
Samuel Felipe Diaz <samuelfdm14@gmail.com>
2025-08-26 22:35:34 -0500

    Refactor directory names

--- src/main/java/eci/ieti/bookingsystem/BookingSystemApplication.java
+++ src/main/java/eci/ieti/bookingsystem/BookingSystemApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package eci.ieti.bookingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class BookingSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BookingSystemApplication.class, args);
 	}
 
 }
--- src/test/java/eci/ieti/bookingsystem/BookingSystemApplicationTests.java
+++ src/test/java/eci/ieti/bookingsystem/BookingSystemApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package eci.ieti.bookingsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class BookingSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
